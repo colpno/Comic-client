d377b63d1407941d9260bcc680f749e465d076b2
Colpno <gvinhh@gmail.com>
2024-09-11 21:01:21 +0700

    create img component for lazy-loaded

--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -2,27 +2,25 @@ import { ImgHTMLAttributes, memo } from 'react';
 import { LazyLoadImage, LazyLoadImageProps } from 'react-lazy-load-image-component';
 import 'react-lazy-load-image-component/src/effects/blur.css';
 
-type ImageAsLazyImageProps = {
-  lazy: boolean;
-} & LazyLoadImageProps;
+import { PlaceholderImage } from '~/images/index.ts';
 
-type ImageAsImageProps = {
+interface ImageAsLazyImageProps extends LazyLoadImageProps {
+  /** If true, and there is a set of this components, the common parent should be wrapped with HOC trackWindowScroll. */
+  lazy: boolean;
+}
+
+interface ImageAsImageProps extends ImgHTMLAttributes<HTMLImageElement> {
   lazy?: never;
-} & ImgHTMLAttributes<HTMLImageElement>;
+}
 
 type Props = ImageAsImageProps | ImageAsLazyImageProps;
 
-/**
- * @param lazy if set to true and there is a set of Image, use HOC trackWindowScroll on the common component.
- */
-function Component({ src, lazy, ...imgProps }: Props) {
+function Image({ lazy, ...imgProps }: Props) {
   if (lazy) {
-    return <LazyLoadImage {...imgProps} src={src} effect="blur" />;
+    return <LazyLoadImage {...imgProps} effect="blur" placeholderSrc={PlaceholderImage} />;
   }
 
-  return <img src={src} {...imgProps} />;
+  return <img {...imgProps} />;
 }
 
-const Image = memo(Component);
-
-export default Image;
+export default memo(Image);
