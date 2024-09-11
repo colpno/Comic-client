caf0d7df0a88d7d85bff276170e7d4818536ceff
Colpno <gvinhh@gmail.com>
2024-09-11 19:08:07 +0700

    add libraries context

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,36 @@
+import CssBaseline from '@mui/material/CssBaseline/CssBaseline';
+import ThemeProvider from '@mui/material/styles/ThemeProvider';
+import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment';
+import {
+  LocalizationProvider,
+} from '@mui/x-date-pickers/LocalizationProvider/LocalizationProvider';
+import { Provider } from 'react-redux';
+
+import { theme } from './libs/mui/theme.ts';
+import store from './libs/redux/store.ts';
+
 function App() {
   return (
-    <div>
+    <ContextWrapper>
       <span className="text-red-500">Hello World!</span>
-    </div>
+    </ContextWrapper>
+  );
+}
+
+interface ContextWrapperProps {
+  children: React.ReactNode;
+}
+
+function ContextWrapper({ children }: ContextWrapperProps) {
+  return (
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <LocalizationProvider dateAdapter={AdapterMoment} adapterLocale="vi">
+          {children}
+        </LocalizationProvider>
+      </ThemeProvider>
+    </Provider>
   );
 }
 
