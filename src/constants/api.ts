d0353cc1c8546794a2b3e12bfafb0743d6cebd63
Colpno <gvinhh@gmail.com>
2024-09-12 16:39:44 +0700

    change server port to 3000

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,3 +1,3 @@
 export const BASE_ENDPOINT = import.meta.env.VITE_BASE_ENDPOINT || '/api/v1';
 export const SERVER_URL =
-  `${import.meta.env.VITE_SERVER_URL}${BASE_ENDPOINT}` || `http://localhost:3001${BASE_ENDPOINT}`;
+  `${import.meta.env.VITE_SERVER_URL}${BASE_ENDPOINT}` || `http://localhost:3000${BASE_ENDPOINT}`;
