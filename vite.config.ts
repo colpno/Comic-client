ebee34575ecf6eacef8722321fda29cbba66584a
Colpno <gvinhh@gmail.com>
2024-09-14 00:12:03 +0700

    tree shaking @mui

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import babel from 'vite-plugin-babel';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths(), babel({ filter: /\.tsx?$/ })],
 });
