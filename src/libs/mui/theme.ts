e8b08054d5e119e4b31a71cdc4d614033a4937b9
Colpno <gvinhh@gmail.com>
2024-09-14 00:06:48 +0700

    add mui locales

--- src/libs/mui/theme.ts
+++ src/libs/mui/theme.ts
@@ -1,3 +1,6 @@
 import { createTheme } from '@mui/material';
+import { viVN } from '@mui/material/locale';
+import { viVN as dataGridViVN } from '@mui/x-data-grid/locales';
+import { viVN as pickerViVN } from '@mui/x-date-pickers/locales';
 
-export const theme = createTheme();
+export const theme = createTheme(viVN, dataGridViVN, pickerViVN);
