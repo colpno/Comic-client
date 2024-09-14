cdcfd41f4cf1a28c332f5b0b614865614b6a8f08
Colpno <gvinhh@gmail.com>
2024-09-14 22:29:29 +0700

    remove dead code

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,12 @@ import { RouterProvider } from 'react-router-dom';
 import '~/libs/moment/config.ts';
 import { Toast } from './components/index.ts';
 import AppContextWrapper from './contexts/AppContextWrapper.tsx';
-import { LoadingPage } from './pages/index.ts';
 import { router } from './routes/index.ts';
 
 function App() {
   return (
     <AppContextWrapper>
       <RouterProvider router={router} />
-      <span className="text-red-500">Hello World!</span>
-      <LoadingPage />
       <Toast />
     </AppContextWrapper>
   );
