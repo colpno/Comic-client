e3c06d73caca663b744a051d7dc716ad4a5850a2
Colpno <gvinhh@gmail.com>
2024-09-14 18:40:27 +0700

    replace React.useId with uuid

--- src/components/Slider/components/SliderSlides.tsx
+++ src/components/Slider/components/SliderSlides.tsx
@@ -1,12 +1,10 @@
-import { useId } from 'react';
 import { SwiperSlide, SwiperSlideProps } from 'swiper/react';
+import { v4 } from 'uuid';
 
 function SliderSlides({ children, ...props }: SwiperSlideProps) {
-  const id = useId();
-
   if (Array.isArray(children)) {
     return children.map((child, index) => (
-      <SwiperSlide {...props} key={`slide-${id}-${index}`}>
+      <SwiperSlide {...props} key={`slide-${v4()}-${index}`}>
         {child}
       </SwiperSlide>
     ));
