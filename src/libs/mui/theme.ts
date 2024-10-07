944f50dd0b80b9381af22c047730a6c962a00631
Colpno <gvinhh@gmail.com>
2024-10-07 20:06:48 +0700

    add some colors

--- src/libs/mui/theme.ts
+++ src/libs/mui/theme.ts
@@ -3,4 +3,18 @@ import { viVN } from '@mui/material/locale';
 import { viVN as dataGridViVN } from '@mui/x-data-grid/locales';
 import { viVN as pickerViVN } from '@mui/x-date-pickers/locales';
 
-export const theme = createTheme(viVN, dataGridViVN, pickerViVN);
+export const theme = createTheme(
+  {
+    palette: {
+      primary: {
+        main: '#f50000',
+      },
+      background: {
+        default: '#fff',
+      },
+    },
+  },
+  viVN,
+  dataGridViVN,
+  pickerViVN
+);
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,13 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
+      colors: {
+        primary: '#f50000',
+      },
+      backgroundColor: {
+        primary: '#fff',
+        secondary: '#f1f3f5',
+      },
       keyframes: {
         loadingPulse: { '100%': { transform: 'scale(10)', opacity: 0 } },
       },
