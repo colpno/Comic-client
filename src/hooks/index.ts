15afc9239fa6dedf58fd7230db00b8e9e6c35247
Colpno <gvinhh@gmail.com>
2024-10-07 22:07:01 +0700

    add a scroll tracking hook

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,2 +1,3 @@
 export { useDeviceWatcher } from './useDeviceWatcher.ts';
 export { useRouteMatch } from './useRouteMatch.ts';
+export { useScroll } from './useScroll.ts';
---
+++ src/hooks/useScroll.ts
@@ -0,0 +1,16 @@
+import { useEffect } from 'react';
+
+export type UseOnScrollCallback = (event: Event) => void;
+
+/**
+ * Hook that listens to the scroll event on the window.
+ * @param callback The function to call when the scroll event is triggered.
+ */
+export const useScroll = (callback: UseOnScrollCallback) => {
+  useEffect(() => {
+    window.addEventListener('scroll', callback);
+    return () => {
+      window.removeEventListener('scroll', callback);
+    };
+  }, [callback]);
+};
