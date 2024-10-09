48c3f757135defb35eeee20346efb397de452a48
Colpno <gvinhh@gmail.com>
2024-10-09 14:13:43 +0700

    add horizontal padding

--- src/layouts/components/Footer.tsx
+++ src/layouts/components/Footer.tsx
@@ -2,7 +2,7 @@ function Footer() {
   return (
     <footer
       id="footer"
-      className="flex flex-col items-center justify-center py-10 bg-secondary text-[#868e96] text-sm"
+      className="flex flex-col items-center justify-center py-10 px-1 sm:px-2 bg-secondary text-[#868e96] text-sm"
     >
       © Comic Corporation. All rights reserved.
     </footer>
