e10500a774e7c13cafbbaa328990d6c5a4eba827
Colpno <gvinhh@gmail.com>
2024-10-12 14:57:35 +0700

    rename constant

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -1,11 +1,11 @@
 import { createApi } from '@reduxjs/toolkit/query/react';
 
 import { SERVER_URL } from '~/constants/api.ts';
-import { REDUCER_PATH_API } from '~/constants/redux.ts';
+import { REDUCER_PATH_BASE } from '~/constants/redux.ts';
 import baseQuery from '~/libs/axios/baseQuery.ts';
 
 const api = createApi({
-  reducerPath: REDUCER_PATH_API,
+  reducerPath: REDUCER_PATH_BASE,
   baseQuery: baseQuery({ baseURL: SERVER_URL }),
   endpoints: () => ({}),
   refetchOnReconnect: true,
--- src/constants/redux.ts
+++ src/constants/redux.ts
@@ -1,3 +1,3 @@
-export const REDUCER_PATH_API = 'api';
+export const REDUCER_PATH_BASE = 'api';
 export const REDUCER_PATH_COMMON = 'common';
 export const REDUCER_PATH_AUTH = 'auth';
