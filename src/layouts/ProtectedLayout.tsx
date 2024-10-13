fc20bd18cafb45daaa39613e3c9c046d6f4f47d1
Colpno <gvinhh@gmail.com>
2024-10-14 00:29:09 +0700

    remove dead code

--- src/layouts/ProtectedLayout.tsx
+++ src/layouts/ProtectedLayout.tsx
@@ -2,12 +2,11 @@ import { useSelector } from 'react-redux';
 import { Outlet } from 'react-router-dom';
 
 import { RootState } from '~/libs/redux/store.ts';
-import { LoginPage } from '~/pages/index.ts';
 
 function ProtectedLayout() {
   const isLoggedIn = useSelector((state: RootState) => state.auth.isLoggedIn);
 
-  if (!isLoggedIn) return <LoginPage />;
+  if (!isLoggedIn) return null;
 
   return <Outlet />;
 }
--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,6 +1,2 @@
-import { lazy } from 'react';
-
-export const LoginPage = lazy(() => import('./LoginPage.tsx'));
-
 export { default as ErrorPage } from './error-page/ErrorPage.tsx';
 export { default as HomePage } from './HomePage.tsx';
