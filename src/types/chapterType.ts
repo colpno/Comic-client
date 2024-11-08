ca5d1b0057b00f835c55f57ef9afd4f98dd0000f
Colpno <gvinhh@gmail.com>
2024-11-08 14:28:57 +0700

    add data types

---
+++ src/types/chapterType.ts
@@ -0,0 +1,15 @@
+export interface Chapter {
+  id: string;
+  title: string;
+  volume?: number;
+  chapter?: number;
+  content?: ChapterContent[];
+  publishAt?: string;
+  readableAt?: string;
+  pages?: number;
+}
+
+interface ChapterContent {
+  data: string;
+  dataSaver?: string;
+}
---
+++ src/types/comicType.ts
@@ -0,0 +1,40 @@
+import { Chapter } from './chapterType.ts';
+import { Artist, Author } from './creatorTypes.ts';
+
+type Status = 'ongoing' | 'completed' | 'hiatus' | 'cancelled';
+type State = 'published' | 'draft';
+type Type = 'manga' | 'manhwa' | 'manhua';
+type ContentRating = 'suggestive';
+type TagGroup = 'theme' | 'genre' | 'format';
+
+interface Tag {
+  id: string;
+  name: string;
+  description: string;
+  group: TagGroup;
+}
+
+export interface Comic {
+  id: string;
+  type: Type;
+  title: string;
+  altTitles?: string[];
+  description?: string;
+  isLocked: boolean;
+  lastVolume?: string;
+  lastChapter?: string;
+  status: Status;
+  year: number;
+  contentRating?: ContentRating;
+  tags: Tag[];
+  state: State;
+  chapterNumbersResetOnNewVolume: boolean;
+  chapters: (Chapter | Chapter['id'])[];
+  latestUploadedChapter?: string;
+  coverImageUrl: string;
+  related?: string[];
+  authors: Author[];
+  artists: Artist[];
+  createdAt: string;
+  updatedAt: string;
+}
