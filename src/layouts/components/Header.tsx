ea2d170a8e9748d0473120d1c23847af08c2f8fa
Colpno <gvinhh@gmail.com>
2024-12-01 15:06:08 +0700

    handle children as function

--- src/layouts/components/Header.tsx
+++ src/layouts/components/Header.tsx
@@ -1,14 +1,21 @@
-import { Container, ContainerProps, useTheme } from '@mui/material';
+import { Container, ContainerProps, Theme, useTheme } from '@mui/material';
 import { HTMLAttributes, memo } from 'react';
 import { Link } from 'react-router-dom';
 
 import { Logo } from '~/components/index.ts';
 import { useDeviceWatcher } from '~/hooks/index.ts';
+import { Device } from '~/hooks/useDeviceWatcher.ts';
 import { cn } from '~/utils/cssUtils.ts';
 import HeaderActions from './HeaderActions/HeaderActions.tsx';
 
+interface FunctionalChildrenProps {
+  device: Device;
+  isMobile: boolean;
+  theme: Theme;
+}
+
 interface Props {
-  children?: React.ReactNode;
+  children?: React.ReactNode | ((props: FunctionalChildrenProps) => React.ReactNode);
   slotProps?: {
     container?: HTMLAttributes<HTMLElement>;
     wrapper?: ContainerProps;
@@ -16,13 +23,36 @@ interface Props {
 }
 
 function Header({ slotProps, children }: Props) {
-  const isMobile = useDeviceWatcher() === 'mobile';
+  const device = useDeviceWatcher();
+  const isMobile = device === 'mobile';
   const theme = useTheme();
+
+  const renderChildren = () => {
+    if (typeof children === 'function') {
+      return children({ device, isMobile, theme });
+    }
+
+    if (children) {
+      return children;
+    }
+
+    return (
+      <>
+        <Link to="/">
+          {isMobile ? (
+            <Logo variant="small" fill={theme.palette.primary.main} />
+          ) : (
+            <Logo width={110} height={27} fill={theme.palette.primary.main} />
+          )}
+        </Link>
+        <HeaderActions />
+      </>
+    );
+  };
 
   return (
     <header
       {...slotProps?.container}
-      id="header"
       className={cn(
         'fixed top-0 left-0 right-0 z-header bg-primary',
         slotProps?.container?.className
@@ -36,18 +66,7 @@ function Header({ slotProps, children }: Props) {
           slotProps?.wrapper?.className
         )}
       >
-        {children ?? (
-          <>
-            <Link to="/">
-              {isMobile ? (
-                <Logo variant="small" fill={theme.palette.primary.main} />
-              ) : (
-                <Logo width={110} height={27} fill={theme.palette.primary.main} />
-              )}
-            </Link>
-            <HeaderActions />
-          </>
-        )}
+        {renderChildren()}
       </Container>
     </header>
   );
