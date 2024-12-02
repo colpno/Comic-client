5014aefeeee011e63e1c7c2a92af56b842c0c70b
Colpno <gvinhh@gmail.com>
2024-12-02 22:09:08 +0700

    add placeholder image

--- src/features/cards/ComicCard.tsx
+++ src/features/cards/ComicCard.tsx
@@ -1,11 +1,19 @@
 import Image from '~/components/Image.tsx';
 import Typography from '~/components/Typography.tsx';
+import { PlaceholderImage } from '~/images/index.ts';
 import { Comic } from '~/types/comicType.ts';
 
 function ComicCard(comic: Comic) {
   return (
     <figure>
-      <Image src={comic.coverImageUrl} alt={comic.title} className="rounded-md" />
+      <Image
+        src={comic.coverImageUrl}
+        alt={comic.title}
+        className="rounded-md"
+        onError={({ currentTarget }) => {
+          currentTarget.src = PlaceholderImage;
+        }}
+      />
       <figcaption className="mt-2">
         <Typography fontWeight={500} className="line-clamp-1">
           {comic.title}
