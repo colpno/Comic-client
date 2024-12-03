dd12df7c73c31494cdd03226c90fe6dde85ef440
Colpno <gvinhh@gmail.com>
2024-12-03 20:20:31 +0700

    make changes to ui

--- src/layouts/ReadingLayout/ReadingLayout.tsx
+++ src/layouts/ReadingLayout/ReadingLayout.tsx
@@ -1,59 +1,109 @@
 import { useState } from 'react';
 import { FaChevronLeft, FaChevronRight } from 'react-icons/fa6';
+import { HiBars3BottomLeft } from 'react-icons/hi2';
 import { PiBooks } from 'react-icons/pi';
-import { Link, Outlet } from 'react-router-dom';
+import { Link, Outlet, useParams } from 'react-router-dom';
 
 import { Button, Logo, Typography } from '~/components/index.ts';
-import { ROUTE_LIBRARY } from '~/constants/routeConstants.ts';
-import { useScroll } from '~/hooks/useScroll.ts';
-import { heights } from '~/utils/cssUtils.ts';
+import { getComicRoute, ROUTE_LIBRARY } from '~/constants/routeConstants.ts';
+import { useDeviceWatcher } from '~/hooks/useDeviceWatcher.ts';
+import { cn } from '~/utils/cssUtils.ts';
 import Footer from '../components/Footer.tsx';
 import BaseHeader from '../components/Header.tsx';
 import HeaderAccountButton from '../components/HeaderActions/HeaderAccountButton.tsx';
-import { ReadingLayoutContextProvider, ReadingLayoutContextType } from './ReadingLayoutContext.ts';
+import {
+  ReadingLayoutContextProvider,
+  ReadingLayoutContextType,
+  useReadingLayoutContext,
+} from './ReadingLayoutContext.ts';
 
 function Header() {
+  const { headerVisibility } = useReadingLayoutContext();
+  const isMobile = useDeviceWatcher() === 'mobile';
+  const { comicId } = useParams();
+  const comicPageHref = comicId ? getComicRoute(comicId) : undefined;
+
   return (
-    <BaseHeader>
-      {({ isMobile, theme }) => (
-        <>
+    <>
+      <BaseHeader
+        slotProps={{
+          container: {
+            className: cn(
+              'shadow-[0_2px_4px_rgba(0,0,0,0.05)] transition-[opacity] ease-out',
+              !headerVisibility && 'opacity-0'
+            ),
+          },
+        }}
+      >
+        {({ isMobile, theme }) => (
+          <>
+            <nav>
+              <Link to="/">
+                {isMobile ? (
+                  <Logo variant="small" fill={theme.palette.primary.main} />
+                ) : (
+                  <Logo width={110} height={27} fill={theme.palette.primary.main} />
+                )}
+              </Link>
+            </nav>
+            {!isMobile && (
+              <nav className="flex items-center gap-2 md:gap-4">
+                <Button as="iconButton" color="inherit" title="Go to the previous">
+                  <FaChevronLeft fontSize={18} />
+                </Button>
+                <Typography variant="h6">2</Typography>
+                <Button as="iconButton" color="inherit" title="Go to the next">
+                  <FaChevronRight fontSize={18} />
+                </Button>
+              </nav>
+            )}
+            <nav>
+              <Button as="iconButton" href={ROUTE_LIBRARY} color="inherit" title="Library">
+                <PiBooks />
+              </Button>
+              <HeaderAccountButton />
+            </nav>
+          </>
+        )}
+      </BaseHeader>
+      {isMobile && (
+        <div
+          className={cn(
+            'fixed bottom-0 left-0 right-0 flex items-center justify-between bg-main shadow-[0_-2px_4px_rgba(0,0,0,0.1)] z-header transition-[opacity] ease-out',
+            !headerVisibility && 'opacity-0'
+          )}
+        >
           <nav>
-            <Link to="/">
-              {isMobile ? (
-                <Logo variant="small" fill={theme.palette.primary.main} />
-              ) : (
-                <Logo width={110} height={27} fill={theme.palette.primary.main} />
-              )}
-            </Link>
+            <Button
+              as="iconButton"
+              href={comicPageHref}
+              color="inherit"
+              title="Go back to the comic page"
+            >
+              <HiBars3BottomLeft />
+            </Button>
           </nav>
+          <div className="flex-1 pl-2 pr-4 line-clamp-1">
+            <Typography>Chapter 2</Typography>
+          </div>
           <nav className="flex items-center gap-2 md:gap-4">
             <Button as="iconButton" color="inherit" title="Go to the previous">
               <FaChevronLeft fontSize={18} />
             </Button>
-            <Typography variant="h6">2</Typography>
             <Button as="iconButton" color="inherit" title="Go to the next">
               <FaChevronRight fontSize={18} />
             </Button>
           </nav>
-          <nav>
-            <Button as="iconButton" href={ROUTE_LIBRARY} color="inherit" title="Library">
-              <PiBooks />
-            </Button>
-            <HeaderAccountButton />
-          </nav>
-        </>
+        </div>
       )}
-    </BaseHeader>
+    </>
   );
 }
 
 function ReadingLayout() {
   const [visibility, setVisibility] = useState(true);
 
-  useScroll(() => setVisibility(!(window.scrollY > parseInt(heights.header))));
-
-  const toggleVisibility = () =>
-    window.scrollY > parseInt(heights.header) && setVisibility((prev) => !prev);
+  const toggleVisibility = () => setVisibility((prev) => !prev);
 
   const contextValues: ReadingLayoutContextType = {
     headerVisibility: visibility,
@@ -64,8 +114,8 @@ function ReadingLayout() {
   return (
     <ReadingLayoutContextProvider value={contextValues}>
       <div className="flex flex-col min-h-dvh">
-        {visibility && <Header />}
-        <main className="flex-1 pb-16 pt-header md:pt-header-md">
+        <Header />
+        <main className="flex-1 pb-16">
           <Outlet />
         </main>
         <Footer />
--- src/pages/ReadingPage/ReadingPage.tsx
+++ src/pages/ReadingPage/ReadingPage.tsx
@@ -12,12 +12,15 @@ import { cn } from '~/utils/cssUtils.ts';
 import ReadingPageSlider from './components/ReadingPageSlider.tsx';
 
 const images = faker.helpers.multiple<Chapter['content'][number]>(
-  () => ({
-    data: faker.image.urlPicsumPhotos(),
-    dataSaver: faker.image.urlPlaceholder(),
-  }),
+  () => {
+    const image = faker.image.urlPicsumPhotos();
+    return {
+      data: image,
+      dataSaver: image,
+    };
+  },
   {
-    count: faker.helpers.rangeToNumber({ min: 5, max: 10 }),
+    count: faker.helpers.rangeToNumber({ min: 1, max: 1 }),
   }
 );
 
@@ -27,15 +30,17 @@ function ReadingPage() {
   return (
     <div>
       <div onClick={toggleHeaderVisibility}>
-        {images.map((img, i) => (
-          <Image
-            src={img.dataSaver || img.data}
-            onLoad={({ currentTarget }) => (currentTarget.src = img.data)}
-            alt={`Image ${i + 1}`}
-            className="w-full md:w-[728px] mx-auto"
-            key={v4()}
-          />
-        ))}
+        <div className="mx-auto shadow-xl w-full md:w-[728px]">
+          {images.map((img, i) => (
+            <Image
+              src={img.dataSaver || img.data}
+              onLoad={({ currentTarget }) => (currentTarget.src = img.data)}
+              alt={`Image ${i + 1}`}
+              className="w-full md:w-[728px]"
+              key={v4()}
+            />
+          ))}
+        </div>
       </div>
       <Container maxWidth="lg" className="mt-24 md:mt-40">
         <ReadingPageSlider>
