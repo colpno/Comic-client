af35feb5891da6916ae666c1ced9f7c6d42a230b
Colpno <gvinhh@gmail.com>
2024-12-05 19:09:04 +0700

    add Loading to wait for all images to load

--- src/pages/ReadingPage/components/ReadingPageImages.tsx
+++ src/pages/ReadingPage/components/ReadingPageImages.tsx
@@ -1,4 +1,7 @@
-import Image from '~/components/Image.tsx';
+import { useEffect, useState } from 'react';
+
+import ImageComponent from '~/components/Image.tsx';
+import Loading from '~/components/Loading.tsx';
 import { Chapter } from '~/types/chapterType.ts';
 
 interface Props {
@@ -7,15 +10,38 @@ interface Props {
 }
 
 function ReadingPageImages({ images, id }: Props) {
-  return images.map((img, i) => (
-    <Image
-      src={img.dataSaver || img.data}
-      onLoad={({ currentTarget }) => (currentTarget.src = img.data)}
-      alt={`Image ${i + 1}`}
-      className="w-full md:w-[728px]"
-      key={`${id}-image-${i + 1}`}
-    />
-  ));
+  const [imagesLoaded, setImagesLoaded] = useState(false);
+
+  // Wait for all images to load
+  useEffect(() => {
+    const imagePromises = images.map((src) => {
+      return new Promise((resolve) => {
+        const img = new Image();
+        img.src = src.data;
+        img.onload = resolve;
+        img.onerror = resolve;
+      });
+    });
+
+    Promise.all(imagePromises).then(() => {
+      setImagesLoaded(true);
+    });
+  }, [images]);
+
+  return (
+    <>
+      {!imagesLoaded && <Loading />}
+      {images.map((img, i) => (
+        <ImageComponent
+          src={img.dataSaver || img.data}
+          onLoad={({ currentTarget }) => (currentTarget.src = img.data)}
+          alt={`Page ${i + 1}`}
+          className="w-full md:w-[728px]"
+          key={`${id}-image-${i + 1}`}
+        />
+      ))}
+    </>
+  );
 }
 
 export default ReadingPageImages;
