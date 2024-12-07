1afbe95c0cc90388d72ed647e10250bd93a0093e
Colpno <gvinhh@gmail.com>
2024-12-07 16:21:21 +0700

    responsive body1 font size

--- src/components/Typography.tsx
+++ src/components/Typography.tsx
@@ -2,6 +2,8 @@ import { Typography as MUITypography, TypographyProps as MUITypographyProps } fr
 import { memo } from 'react';
 import { Link } from 'react-router-dom';
 
+import { cn } from '~/utils/cssUtils.ts';
+
 type TypoAsLinkProps = MUITypographyProps & {
   href: string;
   externalLink?: false;
@@ -14,26 +16,31 @@ type TypoAsExternalLinkProps = MUITypographyProps & {
 
 type Props = MUITypographyProps | TypoAsLinkProps | TypoAsExternalLinkProps;
 
-function Typography(props: Props) {
+function Typography({ className, ...props }: Props) {
+  const classes =
+    !props.variant || props.variant === 'body1'
+      ? cn('!text-sm md:!text-base', className)
+      : className;
+
   if ('href' in props) {
     const { href, externalLink, ...componentProps } = props;
 
     if (externalLink) {
       return (
         <a href={href}>
-          <MUITypography {...componentProps} />
+          <MUITypography {...componentProps} className={classes} />
         </a>
       );
     }
 
     return (
       <Link to={href}>
-        <MUITypography {...componentProps} />
+        <MUITypography {...componentProps} className={classes} />
       </Link>
     );
   }
 
-  return <MUITypography {...props} />;
+  return <MUITypography {...props} className={classes} />;
 }
 
 export default memo(Typography);
