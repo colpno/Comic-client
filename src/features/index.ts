e5a57ea9f44fffc080e84e4591ff18e51ca42669
Colpno <gvinhh@gmail.com>
2024-12-07 16:22:03 +0700

    remove redundant file

--- src/features/index.ts
+++ src/features/index.ts
@@ -1,4 +1,3 @@
 export { default as CardList } from './cards/CardList.tsx';
 export { default as ComicCard } from './cards/ComicCard.tsx';
-export { default as ComicCoverCard } from './cards/ComicCoverCard.tsx';
 export { default as ComicHorizontalCard } from './cards/ComicHorizontalCard.tsx';
