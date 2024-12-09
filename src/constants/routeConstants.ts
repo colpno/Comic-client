024f82d25b292a3df1fe8f70cf1b0f93ca8fbde1
Colpno <gvinhh@gmail.com>
2024-12-09 21:28:35 +0700

    change the params type

--- src/constants/routeConstants.ts
+++ src/constants/routeConstants.ts
@@ -1,5 +1,3 @@
-import { RankingPageCategory } from '~/types/pagesTypes.ts';
-
 export const ROUTE_HOME = '/' as const;
 export const ROUTE_LIBRARY = '/library' as const;
 export const ROUTE_GENRES = '/genres' as const;
@@ -7,7 +5,11 @@ export const ROUTE_DAILY_COMIC = '/daily' as const;
 export const ROUTE_RANKING = '/ranking' as const;
 
 export const getComicRoute = (comicId: string) => `/comics/${comicId}`;
+
 export const getComicReadingRoute = (comicId: string, chapterNumber: string | number) =>
   `${getComicRoute(comicId)}/reading/${chapterNumber}`;
-export const getRankingRoute = (category: RankingPageCategory = 'all') =>
-  `/ranking?category=${category}`;
+
+export const getRankingRoute = (category?: string) => {
+  if (!category) return ROUTE_RANKING;
+  return `${ROUTE_RANKING}?category=${category}`;
+};
