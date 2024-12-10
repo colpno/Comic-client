b62e43b11da108a7b82ebaf64e71eb7d80807f6f
Colpno <gvinhh@gmail.com>
2024-12-10 20:15:20 +0700

    add defaultValue for an alternative value for title

--- src/layouts/MenuLayout/components/MenuLayoutPageTitle.tsx
+++ src/layouts/MenuLayout/components/MenuLayoutPageTitle.tsx
@@ -6,22 +6,23 @@ import { toSentenceCase } from '~/utils/converters.ts';
 
 interface Props {
   urlParam: string;
+  defaultValue?: string;
   onParamChange: (category: string) => void;
 }
 
-function MenuLayoutPageTitle({ onParamChange, urlParam }: Props) {
+function MenuLayoutPageTitle({ onParamChange, urlParam, defaultValue }: Props) {
   const [searchParams] = useSearchParams();
-  const titleParam = searchParams.get(urlParam);
+  const value = searchParams.get(urlParam) || defaultValue;
 
   useEffect(() => {
-    if (titleParam) onParamChange(titleParam);
-  }, [titleParam, onParamChange]);
+    if (value) onParamChange(value);
+  }, [value, onParamChange]);
 
-  if (!titleParam) return null;
+  if (!value) return null;
 
   return (
     <Typography component="h2" variant="h4" className="!mb-8 !text-3xl md:!text-4xl">
-      {toSentenceCase(titleParam)}
+      {toSentenceCase(value)}
     </Typography>
   );
 }
