cfa564e9434b0cd23353c1a7249a010c185a05b7
Colpno <gvinhh@gmail.com>
2024-12-13 16:41:48 +0700

    fixed adornments and added clear button

--- src/components/form-controls/base-controls/TextInput.tsx
+++ src/components/form-controls/base-controls/TextInput.tsx
@@ -1,7 +1,7 @@
 import { TextField, TextFieldProps as MUITextFieldProps } from '@mui/material';
 import { forwardRef, ReactNode, Ref, useState } from 'react';
 
-import { ShowPasswordAdornment } from '../components/ShowPasswordAdornment.tsx';
+import { ClearAdornment, PasswordAdornment } from '../components/adornments.tsx';
 
 type TextInputChangeEvent = React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>;
 
@@ -10,10 +10,29 @@ interface Props extends Omit<MUITextFieldProps, 'name' | 'onChange'> {
   onChange?: (value: string) => void;
   label?: ReactNode;
   uppercase?: boolean;
+  clearable?: boolean;
 }
 
 const TextInput = forwardRef(
-  ({ uppercase, required, value, onChange, type, ...props }: Props, ref: Ref<HTMLDivElement>) => {
+  (
+    {
+      uppercase,
+      required,
+      value,
+      onChange,
+      type,
+      onBlur,
+      onFocus,
+      clearable = false,
+      ...props
+    }: Props,
+    ref: Ref<HTMLDivElement>
+  ) => {
+    const [adornments, setAdornments] = useState<Record<string, boolean>>({
+      password: false,
+      clear: false,
+    });
+
     const handleChange = (e: TextInputChangeEvent) => {
       let value = e.target.value;
       if (uppercase) {
@@ -24,19 +43,42 @@ const TextInput = forwardRef(
 
     const [showPassword, setShowPassword] = useState(false);
     const handleClickShowPassword = () => setShowPassword((show) => !show);
-    let endAdornment = null;
-    if (type === 'password') {
-      endAdornment = (
-        <ShowPasswordAdornment onClick={handleClickShowPassword} showPassword={showPassword} />
+
+    const handleFocus = (e: React.FocusEvent<HTMLInputElement>) => {
+      setAdornments({
+        password: type === 'password',
+        clear: clearable,
+      });
+      onFocus?.(e);
+    };
+
+    const handleBlur = (e: React.FocusEvent<HTMLInputElement>) => {
+      setAdornments({ password: false, clear: false });
+      onBlur?.(e);
+    };
+
+    const renderEndAdornment = () => {
+      const showPasswordAdornment = type === 'password' && adornments.password;
+      const showClearAdornment = clearable && adornments.clear;
+
+      return (
+        <>
+          {showClearAdornment && <ClearAdornment onClick={() => onChange?.('')} />}
+          {showPasswordAdornment && (
+            <PasswordAdornment onClick={handleClickShowPassword} show={showPassword} />
+          )}
+        </>
       );
-    }
+    };
 
     return (
       <TextField
         {...props}
+        onFocus={handleFocus}
+        onBlur={handleBlur}
         slotProps={{
           ...props.slotProps,
-          input: { endAdornment, ...props.slotProps?.input },
+          input: { endAdornment: renderEndAdornment(), ...props.slotProps?.input },
           inputLabel: { required },
         }}
         hidden={type === 'hidden' || props.hidden}
---
+++ src/components/form-controls/components/adornments.tsx
@@ -0,0 +1,24 @@
+import { MdClose, MdVisibility, MdVisibilityOff } from 'react-icons/md';
+
+import { Button } from '~/components/index.ts';
+
+interface AdornmentProps {
+  onClick: () => void;
+  show?: boolean;
+}
+
+export function PasswordAdornment({ onClick, show }: AdornmentProps) {
+  return (
+    <Button as="iconButton" onClick={onClick}>
+      {show ? <MdVisibilityOff size={22} /> : <MdVisibility color="disabled" size={22} />}
+    </Button>
+  );
+}
+
+export function ClearAdornment({ onClick }: AdornmentProps) {
+  return (
+    <Button as="iconButton" onClick={onClick}>
+      <MdClose size={20} />
+    </Button>
+  );
+}
