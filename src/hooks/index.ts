ccd64db6f567799285443d02305800f1525e4bc6
Colpno <gvinhh@gmail.com>
2024-12-14 18:55:13 +0700

    create a hook for observing keys pressing

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,5 +1,6 @@
 export { useClickOutside } from './useClickOutside.ts';
 export { useCustomSliderNavigators } from './useCustomSliderNavigators.ts';
 export { useDeviceWatcher } from './useDeviceWatcher.ts';
+export { useKey } from './useKey.ts';
 export { useRouteMatch } from './useRouteMatch.ts';
 export { useScroll } from './useScroll.ts';
