9c28ed00187e84b68eb7aa64c02c962ed85b5dfe
Colpno <gvinhh@gmail.com>
2024-12-19 21:21:21 +0700

    hook for debouncing value

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,5 +1,6 @@
 export { useClickOutside } from './useClickOutside.ts';
 export { useCustomSliderNavigators } from './useCustomSliderNavigators.ts';
+export { useDebounce } from './useDebounce.ts';
 export { useDeviceWatcher } from './useDeviceWatcher.ts';
 export { useKey } from './useKey.ts';
 export { useRouteMatch } from './useRouteMatch.ts';
---
+++ src/hooks/useDebounce.ts
@@ -0,0 +1,15 @@
+import { useEffect, useState } from 'react';
+
+import { PrimitiveValue } from '~/types/commonTypes.ts';
+
+export const useDebounce = <T extends PrimitiveValue>(value: T, delay: number) => {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+
+  useEffect(() => {
+    const handle = setTimeout(() => setDebouncedValue(value), delay);
+
+    return () => clearTimeout(handle);
+  }, [value]);
+
+  return debouncedValue;
+};
