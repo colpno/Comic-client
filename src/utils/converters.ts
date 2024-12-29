4530881d485df1e5da531c6cf0613ba940adbabb
Colpno <gvinhh@gmail.com>
2024-12-29 18:56:07 +0700

    change toDate default format

--- src/utils/converters.ts
+++ src/utils/converters.ts
@@ -1,5 +1,5 @@
 import moment from 'moment';
 
-export const toDate = (date: string, format: string = 'YYYY-MM-DD') => moment(date).format(format);
+export const toDate = (date: string, format: string = 'MM-DD-YYYY') => moment(date).format(format);
 
 export const toSentenceCase = (str: string) => str.charAt(0).toUpperCase() + str.slice(1);
