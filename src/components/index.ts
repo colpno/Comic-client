0249fd7e4fc8c36b0283b9a87968594e26d7e02b
Colpno <gvinhh@gmail.com>
2024-12-29 18:57:57 +0700

    re-export named exports relating to Table

--- src/components/index.ts
+++ src/components/index.ts
@@ -25,6 +25,7 @@ export { default as Pagination } from './Pagination.tsx';
 export { default as ComicSlider } from './sliders/ComicSlider.tsx';
 export { default as Slider } from './sliders/Slider.tsx';
 export { default as Tab } from './Tab.tsx';
+export * from './Table/Table.tsx';
 export { default as Tabs } from './Tabs/Tabs.tsx';
 export { default as Toast } from './Toast.tsx';
 export { default as Typography } from './Typography.tsx';
