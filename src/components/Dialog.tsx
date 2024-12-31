61d09202f3c97cab0f4d147ef531773278c3efd6
Colpno <gvinhh@gmail.com>
2024-12-31 19:17:55 +0700

    add adding extra props prop

--- src/components/Dialog.tsx
+++ src/components/Dialog.tsx
@@ -2,6 +2,7 @@ import { Dialog as MUIDialog, DialogProps as MUIDialogProps } from '@mui/materia
 import { memo } from 'react';
 
 import { useDeviceWatcher } from '~/hooks/useDeviceWatcher.ts';
+import { ButtonAsButtonProps } from '~/types/formControlTypes.ts';
 import { cn } from '~/utils/cssUtils.ts';
 import { Button, Typography } from './index.ts';
 
@@ -13,7 +14,8 @@ interface DialogAsContainer extends BaseProps {
   variant: 'container';
 }
 
-interface DialogAsConfirm extends Omit<BaseProps, 'title'> {
+interface DialogAsConfirm extends Omit<BaseProps, 'title' | 'slotProps'> {
+  /** @default confirm */
   variant?: 'confirm';
   title: string;
   onAccept?: () => void;
@@ -27,6 +29,14 @@ interface DialogAsConfirm extends Omit<BaseProps, 'title'> {
     title?: string;
     content?: string;
     actions?: string;
+  };
+  slotProps?: Omit<BaseProps['slotProps'], 'title' | 'content' | 'actions'> & {
+    title?: React.ComponentProps<typeof Typography>;
+    content?: React.HTMLAttributes<HTMLDivElement>;
+    actions?: React.HTMLAttributes<HTMLDivElement>;
+    'actions.accept'?: ButtonAsButtonProps;
+    'actions.reject'?: ButtonAsButtonProps;
+    'actions.close'?: ButtonAsButtonProps;
   };
 }
 
@@ -56,31 +66,54 @@ function ConfirmDialog({
   ...props
 }: DialogAsConfirm) {
   const isMobile = useDeviceWatcher() === 'mobile';
+  const {
+    title: titleProps,
+    content: contentProps,
+    actions: actionsProps,
+    'actions.accept': actionsAcceptProps,
+    'actions.close': actionsCloseProps,
+    'actions.reject': actionsRejectProps,
+    ...slotProps
+  } = props.slotProps || {};
 
   return (
     <MUIDialog
       {...props}
+      slotProps={slotProps}
       onClose={onClose}
       fullScreen={isMobile}
       className={cn('z-dialog', className)}
     >
-      <div className={cn('p-3 border-b text-primary-500', classes?.title)}>
-        {overrideUI?.title || <Typography variant="h5">{title}</Typography>}
-      </div>
-      <div className={cn('px-6 overflow-y-auto shadow-inner py-9 text-main', classes?.content)}>
+      {overrideUI?.title || (
+        <Typography
+          {...titleProps}
+          variant="h5"
+          className={cn('p-3 border-b text-primary-500', classes?.title)}
+        >
+          {title}
+        </Typography>
+      )}
+      <div
+        {...contentProps}
+        className={cn('px-6 overflow-y-auto shadow-inner py-9 text-main', classes?.content)}
+      >
         {children}
       </div>
-      <div className={cn('p-3 text-right border-t', classes?.actions)}>
+      <div {...actionsProps} className={cn('p-3 text-right border-t', classes?.actions)}>
         {overrideUI?.actions || (
           <>
-            {onAccept && <Button onClick={onAccept}>Yes</Button>}
+            {onAccept && (
+              <Button {...actionsAcceptProps} onClick={onAccept}>
+                Yes
+              </Button>
+            )}
             {onReject && (
-              <Button variant="outlined" onClick={onReject}>
+              <Button {...actionsRejectProps} variant="outlined" onClick={onReject}>
                 No
               </Button>
             )}
             {onClose && (
-              <Button variant="outlined" onClick={onClose}>
+              <Button {...actionsCloseProps} variant="outlined" onClick={onClose}>
                 Close
               </Button>
             )}
