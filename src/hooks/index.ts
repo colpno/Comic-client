62807895a037c0964e6b9c621eff3805604b2d15
Colpno <gvinhh@gmail.com>
2025-01-03 17:52:50 +0700

    create a hook to observe element intersection

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -3,6 +3,7 @@ export { useCustomSliderNavigators } from './useCustomSliderNavigators.ts';
 export { useDebounce } from './useDebounce.ts';
 export { useDeviceWatcher } from './useDeviceWatcher.ts';
 export { useKey } from './useKey.ts';
+export { useObserver } from './useObserver.ts';
 export { usePopup } from './usePopup.ts';
 export { useRouteMatch } from './useRouteMatch.ts';
 export { useScroll } from './useScroll.ts';
---
+++ src/hooks/useObserver.ts
@@ -0,0 +1,48 @@
+import { useEffect, useRef, useState } from 'react';
+
+type Element = HTMLElement | null;
+
+type UseObserverReturnedValue = {
+  /** Pass to ref prop. */
+  setElementRef: React.Dispatch<React.SetStateAction<Element>>;
+};
+
+type UseObserver = (executeFC?: () => void, removeFC?: () => void) => UseObserverReturnedValue;
+
+/**
+ * Hook to observe an element.
+ * @param executeFC Callback function to execute when the element is intersecting.
+ * @param removeFC Callback function to execute when the element is not intersecting.
+ */
+export const useObserver: UseObserver = (executeFC, removeFC) => {
+  const [elementRef, setElementRef] = useState<Element>(null);
+
+  const observer = useRef(
+    new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          executeFC?.();
+        } else {
+          removeFC?.();
+        }
+      });
+    })
+  );
+
+  useEffect(() => {
+    const currentElement = elementRef;
+    const currentObserver = observer.current;
+
+    if (currentElement) {
+      currentObserver.observe(currentElement);
+    }
+
+    return () => {
+      if (currentElement) {
+        currentObserver.unobserve(currentElement);
+      }
+    };
+  }, [elementRef]);
+
+  return { setElementRef };
+};
