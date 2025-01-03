8b50ac56fba9dd1187e8748d5c10d5e93a18f986
Colpno <gvinhh@gmail.com>
2025-01-03 15:32:24 +0700

    implement props to pass to main

--- src/layouts/BasicLayout.tsx
+++ src/layouts/BasicLayout.tsx
@@ -1,14 +1,19 @@
 import { Outlet } from 'react-router-dom';
 
+import { cn } from '~/utils/cssUtils.ts';
 import Footer from './components/Footer.tsx';
 import Header from './components/Header.tsx';
 
-function BasicLayout() {
+interface Props extends React.HTMLAttributes<HTMLDivElement> {
+  children?: React.ReactNode;
+}
+
+function BasicLayout({ children, className, ...props }: Props) {
   return (
     <div className="flex flex-col min-h-dvh">
       <Header />
-      <main className="flex-1 pt-header md:pt-header-md">
-        <Outlet />
+      <main {...props} className={cn('flex-1 pt-header md:pt-header-md', className)}>
+        {children ?? <Outlet />}
       </main>
       <Footer />
     </div>
