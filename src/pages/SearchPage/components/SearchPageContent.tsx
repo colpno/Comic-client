8e046854a93b4e2915141afae7c1c2e85dea10a6
Colpno <gvinhh@gmail.com>
2025-01-04 15:35:41 +0700

    implement additional UI for no search results

--- src/pages/SearchPage/components/SearchPageContent.tsx
+++ src/pages/SearchPage/components/SearchPageContent.tsx
@@ -1,4 +1,7 @@
+import Image from '~/components/Image.tsx';
+import { Typography } from '~/components/index.ts';
 import { SearchingComicCard } from '~/features/index.ts';
+import { noSearchSVG } from '~/images/index.ts';
 import { Comic } from '~/types/comicType.ts';
 
 interface Props {
@@ -6,6 +9,17 @@ interface Props {
 }
 
 function SearchPageContent({ items }: Props) {
+  if (items.length === 0) {
+    return (
+      <div>
+        <Image src={noSearchSVG} alt="No search result" className="mx-auto mt-12" />
+        <Typography textAlign="center" variant="h6">
+          No search results...
+        </Typography>
+      </div>
+    );
+  }
+
   return (
     <section className="flex flex-col gap-4">
       {items.map((comic) => (
