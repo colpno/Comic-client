0eb231377c20ecaee0abce0a288b0c91f6e4aaa5
Colpno <gvinhh@gmail.com>
2025-01-04 21:48:50 +0700

    redirect to LoginPage if didn't login

--- src/layouts/ProtectedLayout.tsx
+++ src/layouts/ProtectedLayout.tsx
@@ -1,12 +1,16 @@
 import { useSelector } from 'react-redux';
-import { Outlet } from 'react-router-dom';
+import { Navigate, Outlet } from 'react-router-dom';
 
+import { ROUTE_LOGIN } from '~/constants/routeConstants.ts';
 import { RootState } from '~/libs/redux/store.ts';
 
 function ProtectedLayout() {
   const isLoggedIn = useSelector((state: RootState) => state.auth.isLoggedIn);
 
-  if (!isLoggedIn) return null;
+  if (!isLoggedIn) {
+    const href = `${ROUTE_LOGIN}?redirect=${window.location.pathname}`;
+    return <Navigate to={href} />;
+  }
 
   return <Outlet />;
 }
