4298b343950d8f288c8359a8633362493d6c8af2
Colpno <gvinhh@gmail.com>
2025-01-04 18:31:54 +0700

    use data from faker in CardRow

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,22 +1,33 @@
+import { faker } from '@faker-js/faker';
+
 import { generateComics } from '~/database/comics.ts';
 import BannerSlider from './components/HomePageBannerSlider.tsx';
 import CardRow from './components/HomePageCardRow.tsx';
 import Menu from './components/HomePageMenu.tsx';
 
-const comics = generateComics(10);
+const newComics = generateComics(10);
+const comicsByGenre: React.ComponentProps<typeof CardRow>[] = faker.helpers.multiple(
+  () => ({
+    items: generateComics(10),
+    title: faker.commerce.productMaterial(),
+  }),
+  { count: 7 }
+);
+
+function CardRowList() {
+  comicsByGenre[2].itemsPerGroup = 4;
+  comicsByGenre[3].itemsPerGroup = 2;
+  comicsByGenre[5].itemsPerGroup = 4;
+
+  return comicsByGenre.map((props) => <CardRow {...props} />);
+}
 
 function HomePage() {
   return (
     <div>
-      <BannerSlider items={comics} />
+      <BannerSlider items={newComics} />
       <Menu />
-      <CardRow title="Genre 1" items={comics} />
-      <CardRow title="Genre 1" items={comics} />
-      <CardRow title="Genre 1" items={comics} itemsPerGroup={4} />
-      <CardRow title="Genre 1" items={comics} itemsPerGroup={2} />
-      <CardRow title="Genre 1" items={comics} />
-      <CardRow title="Genre 1" items={comics} itemsPerGroup={4} />
-      <CardRow title="Genre 1" items={comics} />
+      <CardRowList />
     </div>
   );
 }
