45ad2c5a295a5be65b018a60ce0d98ed237d5919
Colpno <gvinhh@gmail.com>
2025-01-10 20:33:09 +0700

    remove required method in loginFormSchema

--- src/features/forms/validationSchemas.ts
+++ src/features/forms/validationSchemas.ts
@@ -1,15 +1,13 @@
 import { z } from 'zod';
 
-export const loginFormSchema = z
-  .object({
-    email: z.string().email(),
-    password: z.string().min(10),
-    rememberMe: z
-      .array(z.enum(['true']))
-      .transform((val) => val[0])
-      .optional(),
-  })
-  .required();
+export const loginFormSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(10),
+  rememberMe: z
+    .array(z.enum(['true']))
+    .transform((val) => val[0])
+    .optional(),
+});
 export type LoginFormValues = z.infer<typeof loginFormSchema>;
 
 export const selectiveFilterFormSchema = z.object({
