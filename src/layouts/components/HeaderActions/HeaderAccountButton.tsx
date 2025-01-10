4ee7a4389ad28170c8c822f46cee6b4d4061ad2e
Colpno <gvinhh@gmail.com>
2025-01-10 20:30:16 +0700

    implement UI for mobile and desktop

--- src/layouts/components/HeaderActions/HeaderAccountButton.tsx
+++ src/layouts/components/HeaderActions/HeaderAccountButton.tsx
@@ -1,22 +1,29 @@
 import { IoLogOutOutline } from 'react-icons/io5';
 import { MdClose, MdOutlinePermIdentity } from 'react-icons/md';
 import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 
-import { Button, Popup } from '~/components/index.ts';
+import { Button, Dialog, Popup } from '~/components/index.ts';
+import { ROUTE_HOME } from '~/constants/routeConstants.ts';
 import { LoginFormValues } from '~/features/forms/validationSchemas.ts';
 import { LoginForm } from '~/features/index.ts';
 import { useDeviceWatcher, usePopup } from '~/hooks/index.ts';
-import { login, logout } from '~/libs/redux/slices/authSlice.ts';
-import { RootState, useAppDispatch } from '~/libs/redux/store.ts';
+import { RootState } from '~/libs/redux/store.ts';
 
 function HeaderAccountButton() {
   const { closePopup, open, openPopup, popupRef } = usePopup();
+  const navigate = useNavigate();
   const isLoggedIn = useSelector((state: RootState) => state.auth.isLoggedIn);
-  const dispatch = useAppDispatch();
+  const isMobile = useDeviceWatcher() === 'mobile';
 
   const handleLogout = () => {
+    navigate(ROUTE_HOME);
     // TODO: Implement logout logic
-    dispatch(logout());
+  };
+
+  const handleFormSubmit = async (values: LoginFormValues) => {
+    // TODO: Implement login logic
+    console.log('values:', values);
   };
 
   return (
@@ -30,45 +37,49 @@ function HeaderAccountButton() {
           <MdOutlinePermIdentity />
         </Button>
       )}
-      <Popup
-        open={open}
-        onClose={closePopup}
-        anchorEl={popupRef}
-        anchorOrigin={{ horizontal: 'right' }}
-      >
-        <Form />
-      </Popup>
+      {isMobile ? (
+        <Dialog variant="container" open={open} fullScreen>
+          <MobileForm onClose={closePopup} onSubmit={handleFormSubmit} />
+        </Dialog>
+      ) : (
+        <Popup
+          open={open}
+          onClose={closePopup}
+          anchorEl={popupRef}
+          position={{ horizontal: 'right' }}
+        >
+          <DesktopForm onSubmit={handleFormSubmit} />
+        </Popup>
+      )}
     </div>
   );
 }
 
 export default HeaderAccountButton;
 
-function Form() {
-  const dispatch = useAppDispatch();
-  const isMobile = useDeviceWatcher() === 'mobile';
+interface DesktopFormProps {
+  onSubmit: (values: LoginFormValues) => void;
+}
 
-  const handleFormSubmit = (values: LoginFormValues) => {
-    console.log(values);
-    // TODO: Implement login logic
-
-    // dispatch(login());
-  };
-
-  if (isMobile) {
-    return (
-      <div className="fixed top-0 bottom-0 left-0 right-0 px-6 py-10 overflow-y-scroll bg-main">
-        <Button as="iconButton" title="Close" className="!absolute top-4 right-4">
-          <MdClose size={32} />
-        </Button>
-        <LoginForm onSubmit={handleFormSubmit} />
-      </div>
-    );
-  }
-
+function DesktopForm({ onSubmit }: DesktopFormProps) {
   return (
     <div className="px-6 pt-6 pb-10 shadow-md dark:shadow-gray-900 border dark:border-gray-800 w-[22rem] rounded-xl bg-main">
-      <LoginForm onSubmit={handleFormSubmit} />
+      <LoginForm onSubmit={onSubmit} />
+    </div>
+  );
+}
+
+interface MobileFormProps extends Pick<DesktopFormProps, 'onSubmit'> {
+  onClose: ReturnType<typeof usePopup>['closePopup'];
+}
+
+function MobileForm({ onSubmit, onClose }: MobileFormProps) {
+  return (
+    <div className="relative h-full px-6 py-10 overflow-y-scroll bg-main text-main">
+      <Button as="iconButton" title="Close" className="!absolute top-3 right-3" onClick={onClose}>
+        <MdClose size={32} />
+      </Button>
+      <LoginForm onSubmit={onSubmit} />
     </div>
   );
 }
