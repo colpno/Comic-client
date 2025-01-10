df94194e25b714d3f722ba31fa00b9c5cabe33e6
Colpno <gvinhh@gmail.com>
2025-01-10 20:20:49 +0700

    implement csrf

--- src/libs/axios/baseQuery.ts
+++ src/libs/axios/baseQuery.ts
@@ -1,36 +1,70 @@
 import { BaseQueryFn } from '@reduxjs/toolkit/query/react';
 import axios, { AxiosError, AxiosRequestConfig, Method } from 'axios';
+import { toast } from 'react-toastify';
 
-import { FailedApiResponse, FulfilledResponse } from '~/types/apiTypes';
+import { AUTH_ENDPOINTS } from '~/apis/apiConstants.ts';
+import { cookieCSRFToken } from '~/configs/appConf.ts';
+import { HEADER_CSRF_TOKEN } from '~/constants/commonConstants.ts';
+import { ApiGetCSRFReturnType } from '~/types/apis/authApiTypes.ts';
+import { ApiFailedResponse, ApiFulfilledResponse } from '~/types/apiTypes';
 
-interface ApiRequest {
+axios.defaults.xsrfCookieName = cookieCSRFToken;
+axios.defaults.xsrfHeaderName = HEADER_CSRF_TOKEN;
+axios.defaults.withCredentials = true;
+axios.defaults.withXSRFToken = true;
+
+interface ApiRequest extends Omit<AxiosRequestConfig, 'url' | 'method' | 'data' | 'params'> {
   url: string;
   method?: Method;
   data?: AxiosRequestConfig['data'];
   params?: AxiosRequestConfig['params'];
 }
-type BaseQuery = BaseQueryFn<ApiRequest, FulfilledResponse, FailedApiResponse>;
+type BaseQuery = BaseQueryFn<ApiRequest, ApiFulfilledResponse, ApiFailedResponse>;
 
 const baseQuery =
   ({ baseURL }: { baseURL: string } = { baseURL: '' }): BaseQuery =>
-  async ({ url, method, data, params }, { signal }) => {
+  async ({ url, method, data, params, ...axiosConfigs }, { signal }) => {
+    const mutationMethods: Method[] = ['POST', 'PUT', 'PATCH', 'post', 'put', 'patch'];
+    const isMutatedRequest = mutationMethods.includes(method || 'GET');
+    const axiosRequestConfig: AxiosRequestConfig = {
+      ...axiosConfigs,
+      url: baseURL + url,
+      method,
+      data,
+      params,
+      signal,
+    };
+
     try {
-      const result = await axios<FulfilledResponse>({
-        url: baseURL + url,
-        method,
-        data,
-        params,
-        signal,
-      });
+      // Fetch CSRF token if it's a mutated request
+      if (isMutatedRequest) {
+        const csrfResult = await axios<ApiGetCSRFReturnType>({
+          url: baseURL + AUTH_ENDPOINTS.GET_CSRF(),
+        });
+        const csrfToken = csrfResult.data.data;
+
+        // Set token to header
+        axiosRequestConfig.headers = {
+          ...axiosRequestConfig.headers,
+          [HEADER_CSRF_TOKEN]: csrfToken,
+        };
+      }
+
+      // Fetch data
+      const result = await axios<ApiFulfilledResponse>(axiosRequestConfig);
+
       return { data: result.data };
     } catch (unknownError) {
-      const axiosError = unknownError as AxiosError<FailedApiResponse>;
+      const axiosError = unknownError as AxiosError<ApiFailedResponse>;
 
-      const message =
-        axiosError.response?.data.reason ||
-        'Something went wrong, please contact with any Administrator';
       const status = axiosError.response?.data.code || 500;
       const success = axiosError.response?.data.error || false;
+      const message =
+        axiosError.response?.data.reason ||
+        (unknownError as Error).message ||
+        'Something went wrong, please contact with any Administrator';
+
+      typeof message === 'string' && toast.error(message);
 
       return {
         error: {
