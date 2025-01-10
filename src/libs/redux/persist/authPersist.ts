a39d193cb2d8dc84d73ac2bc82b52b9f14172e26
Colpno <gvinhh@gmail.com>
2025-01-10 15:14:05 +0700

    remove user attribute

--- src/libs/redux/persist/authPersist.ts
+++ src/libs/redux/persist/authPersist.ts
@@ -4,7 +4,7 @@ import storage from 'redux-persist/lib/storage';
 import { REDUCER_PATH_AUTH } from '~/constants/reduxConstants.ts';
 import { AuthReducerState } from '../slices/authSlice.ts';
 
-const whitelist: (keyof AuthReducerState)[] = ['user', 'isLoggedIn'];
+const whitelist: (keyof AuthReducerState)[] = ['isLoggedIn'];
 
 const authPersistConfig: PersistConfig<AuthReducerState> = {
   key: REDUCER_PATH_AUTH,
--- src/libs/redux/slices/authSlice.ts
+++ src/libs/redux/slices/authSlice.ts
@@ -1,17 +1,12 @@
-import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { createSlice } from '@reduxjs/toolkit';
 
 import { REDUCER_PATH_AUTH } from '~/constants/reduxConstants';
-import { User } from '~/types/userTypes';
 
 interface State {
-  user: Pick<User, 'id'> | null;
   isLoggedIn: boolean;
 }
 
-type LoginAction = PayloadAction<Exclude<State['user'], null>>;
-
 const initialState: State = {
-  user: null,
   isLoggedIn: import.meta.env.DEV,
 };
 
@@ -19,12 +14,10 @@ const slice = createSlice({
   name: REDUCER_PATH_AUTH,
   initialState,
   reducers: {
-    login: (state, { payload }: LoginAction) => {
-      state.user = payload;
+    login: (state) => {
       state.isLoggedIn = true;
     },
     logout: (state) => {
-      state.user = null;
       state.isLoggedIn = false;
     },
   },
