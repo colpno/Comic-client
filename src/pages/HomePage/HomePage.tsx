500d5fe7529bb533055c05214078be711d32445d
Colpno <gvinhh@gmail.com>
2025-01-12 18:45:49 +0700

    implement api

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,37 +1,94 @@
-import { faker } from '@faker-js/faker';
+import { useEffect, useState } from 'react';
 import { Helmet } from 'react-helmet-async';
+import { v4 } from 'uuid';
 
-import { generateComics } from '~/database/comics.ts';
+import { useGetComicsQuery } from '~/apis/comicApis.ts';
+import { Comic } from '~/types/comicType.ts';
 import BannerSlider from './components/HomePageBannerSlider.tsx';
 import CardRow from './components/HomePageCardRow.tsx';
+import GenreContainer from './components/HomePageGenreContainer.tsx';
 import Menu from './components/HomePageMenu.tsx';
 
-const newComics = generateComics(10);
+const genres = [
+  'Reincarnation',
+  'Action',
+  'Drama',
+  'Comedy',
+  'Fantasy',
+  'Sci-Fi',
+  'Romance',
+  'Historical',
+  'Monsters',
+  'Slice of Life',
+];
+
+const CARD_ROW_ITEM_LIMIT = 10;
 
 type ComicsByGenre = React.ComponentProps<typeof CardRow> & { id: string };
-const comicsByGenre: ComicsByGenre[] = faker.helpers.multiple(
-  () => ({
-    items: newComics,
-    title: faker.commerce.productMaterial(),
-    id: faker.database.mongodbObjectId(),
-  }),
-  { count: 7 }
-);
+type FilterComicsByGenre = (genres: string[], comics: Comic[]) => ComicsByGenre[];
 
-function CardRowList() {
-  comicsByGenre[2].itemsPerGroup = 4;
-  comicsByGenre[3].itemsPerGroup = 2;
-  comicsByGenre[5].itemsPerGroup = 4;
+const filterComicsByGenre: FilterComicsByGenre = (genres, comics) => {
+  // Initialize comics by genre object
+  let genreComics = genres.reduce((acc, genre) => {
+    acc[genre] = [];
+    return acc;
+  }, {} as Record<string, Comic[]>);
 
-  return comicsByGenre.map(({ id, ...props }) => <CardRow key={id} {...props} />);
-}
+  // Assign comics to genre
+  comics.forEach((comic) => {
+    comic.tags.some(({ name: tag }) => {
+      if (genreComics[tag] && genreComics[tag].length < CARD_ROW_ITEM_LIMIT) {
+        genreComics[tag].push(comic);
+        return true;
+      }
+    });
+  });
+
+  // Convert object to array
+  const comicsByGenre: ReturnType<FilterComicsByGenre> = Object.entries(genreComics).map(
+    ([genre, comics]) => ({
+      items: comics,
+      title: genre,
+      id: v4(),
+    })
+  );
+
+  return comicsByGenre;
+};
 
 function HomePage() {
+  const { data: comics = [] } = useGetComicsQuery({
+    _limit: 100,
+    includedTags: genres,
+    includedTagsMode: 'OR',
+    _embed: 'cover_art',
+  });
+  const [comicsByGenre, setComicsByGenre] = useState<ComicsByGenre[]>([]);
+
+  // Filter comics by genre
+  useEffect(() => {
+    setComicsByGenre(filterComicsByGenre(genres, comics));
+  }, [comics.length, setComicsByGenre]);
+
+  if (!(comics.length > 0 && comicsByGenre.length > 0)) {
+    return null;
+  }
+
   return (
-    <div>
-      <BannerSlider items={newComics} />
+    <div className="pb-16">
+      <BannerSlider items={comics.slice(0, 10)} />
       <Menu />
-      <CardRowList />
+      <CardRow {...comicsByGenre[0]} />
+      <CardRow {...comicsByGenre[1]} />
+      <CardRow {...comicsByGenre[2]} />
+      <CardRow {...comicsByGenre[3]} itemsPerGroup={4} />
+      <CardRow {...comicsByGenre[4]} itemsPerGroup={2} />
+      <GenreContainer />
+      <CardRow {...comicsByGenre[5]} itemsPerGroup={4} />
+      <CardRow {...comicsByGenre[6]} />
+      <CardRow {...comicsByGenre[7]} />
+      <CardRow {...comicsByGenre[8]} />
+      <CardRow {...comicsByGenre[9]} />
       <Helmet>
         <title>Comic</title>
       </Helmet>
