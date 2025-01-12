c1141d319808b2ceedd04ea4042b855df6269950
Colpno <gvinhh@gmail.com>
2025-01-12 18:02:54 +0700

    remove `as const`

--- src/constants/commonConstants.ts
+++ src/constants/commonConstants.ts
@@ -2,7 +2,7 @@ import { Theme } from '~/types/commonTypes.ts';
 
 export const DATE_FORMAT = 'DD/MM/YYYY' as const;
 
-export const PAGINATION_INITIAL_PAGE = 1 as const;
+export const PAGINATION_INITIAL_PAGE = 1;
 
 export const MUI_CONTAINER_MAX_WIDTH = 'md' as const;
 
