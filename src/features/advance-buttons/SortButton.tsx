fda2c13d68e927f6e1a0383bb34de2ff2e2490e2
Colpno <gvinhh@gmail.com>
2025-01-14 20:08:31 +0700

    change the value type to asc and desc

--- src/features/advance-buttons/SortButton.tsx
+++ src/features/advance-buttons/SortButton.tsx
@@ -2,22 +2,27 @@ import { useState } from 'react';
 import { FaSortNumericDown, FaSortNumericUp } from 'react-icons/fa';
 
 import { Button, ButtonAsIconButtonProps } from '~/components/index.ts';
+import { SortOrder } from '~/types/apiTypes.ts';
 
-interface Props extends Partial<Omit<ButtonAsIconButtonProps, 'onChange'>> {
-  onChange: (isAsc: boolean) => void;
+interface Props extends Partial<Omit<ButtonAsIconButtonProps, 'onChange' | 'defaultValue'>> {
+  defaultValue?: SortOrder;
+  onChange: (order: SortOrder) => void;
 }
 
-function SortButton({ onChange, ...props }: Props) {
-  const [isAsc, setIsAsc] = useState(false);
+function SortButton({ onChange, defaultValue, ...props }: Props) {
+  const [order, setOrder] = useState<SortOrder>(defaultValue ?? 'asc');
 
   const toggleSort = () => {
-    setIsAsc((prev) => !prev);
-    onChange(isAsc);
+    setOrder((prev) => {
+      const newValue = prev === 'asc' ? 'desc' : 'asc';
+      onChange(newValue);
+      return newValue;
+    });
   };
 
   return (
     <Button as="iconButton" {...props} onClick={toggleSort}>
-      {isAsc ? <FaSortNumericDown /> : <FaSortNumericUp />}
+      {order === 'asc' ? <FaSortNumericDown /> : <FaSortNumericUp />}
     </Button>
   );
 }
