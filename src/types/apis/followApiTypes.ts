4442c0f4bb8b4056a456477097af3253a55b6ec4
Colpno <gvinhh@gmail.com>
2025-01-14 22:18:02 +0700

    minor change

--- src/types/apis/followApiTypes.ts
+++ src/types/apis/followApiTypes.ts
@@ -4,16 +4,18 @@ import { Follow } from '../followType.ts';
 import { ApiGetComicsParams } from './comicApiTypes.ts';
 
 export type ApiGetFollowReturnType = ApiFulfilledResponse<Follow<string | Comic>[]>;
-export type ApiGetFollowParams = Partial<Omit<Follow, 'following'>> &
-  Omit<GetRequestOperators<Follow>, '_embed' | '_select'> & {
-    follower?: string;
-    following?: Partial<Pick<FilterOperators, 'all' | 'in' | 'nin'>> & Filter<Comic>;
-    _embed?: {
-      path: 'following';
-      match?: Omit<ApiGetComicsParams, '_limit' | '_page' | '_sort' | '_embed'>;
-      populate?: ApiGetComicsParams['_embed'];
-    };
-  };
+export type ApiGetFollowParams = Partial<
+  Omit<Follow, 'following'> &
+    Omit<GetRequestOperators<Follow>, '_embed' | '_select'> & {
+      follower?: string;
+      following?: Partial<Pick<FilterOperators, 'all' | 'in' | 'nin'>> & Filter<Comic>;
+      _embed?: {
+        path: 'following';
+        match?: Omit<ApiGetComicsParams, '_limit' | '_page' | '_sort' | '_embed'>;
+        populate?: ApiGetComicsParams['_embed'];
+      };
+    }
+>;
 
 export type ApiAddFollowParam = Comic['id'];
 
