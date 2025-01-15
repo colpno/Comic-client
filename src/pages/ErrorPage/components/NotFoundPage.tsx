953a0ef89f6438b3fdceb28ade912e67e5f34df2
Colpno <gvinhh@gmail.com>
2025-01-15 16:16:38 +0700

    allow to re-use

--- src/pages/ErrorPage/components/NotFoundPage.tsx
+++ src/pages/ErrorPage/components/NotFoundPage.tsx
@@ -11,31 +11,33 @@ import { noDataSVG } from '~/images/index.ts';
 import { BasicLayout } from '~/layouts/index.ts';
 
 interface Props {
-  error: ErrorResponse;
+  error?: ErrorResponse;
+  title?: string;
 }
 
-function NotFoundPage({ error }: Props) {
-  const { data, status, statusText } = error;
+function NotFoundPage({ error, title = 'Uh oh! You got lost.' }: Props) {
   const navigate = useNavigate();
 
   return (
     <BasicLayout className="flex flex-col items-center justify-center px-2 space-y-3 -translate-y-8">
       <Image src={noDataSVG} alt="Page not found" className="mt-4" />
-      <Typography variant="h2">Uh oh! You got lost.</Typography>
+      <Typography variant="h2">{title}</Typography>
       <div className="space-x-2 !mb-4">
         <Button href={ROUTE_HOME}>Go to home</Button>
         <Button onClick={() => navigate(-1)}>Go back</Button>
       </div>
-      <div className="max-w-[400px]">
-        <Accordion>
-          <AccordionSummary expandIcon={<MdExpandMore />}>
-            <Typography variant="h6" className="line-clamp-1">
-              Detail: {status}, {statusText}.
-            </Typography>
-          </AccordionSummary>
-          <AccordionDetails>{data}</AccordionDetails>
-        </Accordion>
-      </div>
+      {error && (
+        <div className="max-w-[400px]">
+          <Accordion>
+            <AccordionSummary expandIcon={<MdExpandMore />}>
+              <Typography variant="h6" className="line-clamp-1">
+                Detail: {error.status}, {error.statusText}.
+              </Typography>
+            </AccordionSummary>
+            <AccordionDetails>{error.data}</AccordionDetails>
+          </Accordion>
+        </div>
+      )}
       <Helmet>
         <title>404 - Comic</title>
       </Helmet>
