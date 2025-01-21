756eef46c9ef72ec7c58042980b96d6979fab5a5
Colpno <gvinhh@gmail.com>
2025-01-21 11:47:29 +0700

    create .env file

--- src/configs/appConf.ts
+++ src/configs/appConf.ts
@@ -1,6 +1,6 @@
-export const serverBaseEndpoint: string = import.meta.env.VITE_BASE_ENDPOINT || '/api/v1';
+export const serverBaseEndpoint: string = import.meta.env.VITE_SERVER_BASE_ENDPOINT || '/api/v1';
 export const serverUrl: string = import.meta.env.VITE_SERVER_URL
   ? `${import.meta.env.VITE_SERVER_URL}${serverBaseEndpoint}`
   : `http://localhost:3000${serverBaseEndpoint}`;
 
-export const proxyServerUrl = import.meta.env.VITE_SERVER_URL || 'http://localhost:8080';
+export const proxyServerUrl = import.meta.env.VITE_PROXY_SERVER_URL || 'http://localhost:8080';
