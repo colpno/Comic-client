3eaf9409289852a8e945015c4cc659e780be1416
Colpno <gvinhh@gmail.com>
2025-01-23 16:30:55 +0700

    make Props extends TableProps

--- src/pages/HistoryPage/components/HistoryPageTable.tsx
+++ src/pages/HistoryPage/components/HistoryPageTable.tsx
@@ -1,15 +1,16 @@
 import { Table } from '~/components/index.ts';
-import { TableData } from '~/types/index.ts';
+import { TableData, TableProps } from '~/types/index.ts';
 import { getTableDef } from '../historyPageTableDef.tsx';
 
-interface Props {
+interface Props extends Omit<TableProps, 'columns' | 'rows' | 'removable' | 'onRemove'> {
   data: TableData;
   onRemove: Exclude<React.ComponentProps<typeof Table>['onRemove'], undefined>;
 }
 
-function HistoryPageTable({ data, onRemove }: Props) {
+function HistoryPageTable({ data, onRemove, ...props }: Props) {
   return (
     <Table
+      {...props}
       columns={getTableDef()}
       rows={data}
       removable={{ single: true, multiple: true }}
