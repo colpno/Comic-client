75e08055ad512c0b730eb1d5a454cd8a020d82ad
Colpno <gvinhh@gmail.com>
2025-01-23 15:07:02 +0700

    add email for helping

--- src/layouts/components/Footer.tsx
+++ src/layouts/components/Footer.tsx
@@ -1,3 +1,5 @@
+import { Typography } from '~/components/index.ts';
+import { EMAIL_HELPER } from '~/constants/commonConstants.ts';
 import { cn } from '~/utils/cssUtils.ts';
 
 function Footer({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) {
@@ -5,11 +7,14 @@ function Footer({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) {
     <footer
       {...props}
       className={cn(
-        'flex flex-col items-center justify-center py-10 px-1 sm:px-2 bg-sub text-[#868e96] text-sm',
+        'flex flex-col items-center justify-center py-8 px-1 sm:px-2 bg-sub text-[#868e96] text-sm',
         className
       )}
     >
-      © Comic Corporation. All rights reserved.
+      <Typography className="!mb-2 !text-main" copyable>
+        {EMAIL_HELPER}
+      </Typography>
+      <Typography>© Comic Corporation. All rights reserved.</Typography>
     </footer>
   );
 }
