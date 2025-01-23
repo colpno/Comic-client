e58f88020d9653b51d3ab47fc5b03ee753cad6a0
Colpno <gvinhh@gmail.com>
2025-01-23 15:08:22 +0700

    make NotFoundPage suit not-found-data case

--- src/pages/ErrorPage/components/NotFoundPage.tsx
+++ src/pages/ErrorPage/components/NotFoundPage.tsx
@@ -6,9 +6,10 @@ import { ErrorResponse, useNavigate } from 'react-router-dom';
 import Image from '~/components/Image.tsx';
 import { Button } from '~/components/index.ts';
 import Typography from '~/components/Typography.tsx';
+import { EMAIL_HELPER } from '~/constants/commonConstants.ts';
 import { ROUTE_HOME } from '~/constants/routeConstants.ts';
 import { noDataSVG } from '~/images/index.ts';
-import { BasicLayout } from '~/layouts/index.ts';
+import Header from '~/layouts/components/Header.tsx';
 
 interface Props {
   error?: ErrorResponse;
@@ -19,29 +20,39 @@ function NotFoundPage({ error, title = 'Uh oh! You got lost.' }: Props) {
   const navigate = useNavigate();
 
   return (
-    <BasicLayout className="flex flex-col items-center justify-center px-2 space-y-3 -translate-y-8">
-      <Image src={noDataSVG} alt="Page not found" className="mt-4" />
-      <Typography variant="h2">{title}</Typography>
-      <div className="space-x-2 !mb-4">
-        <Button href={ROUTE_HOME}>Go to home</Button>
-        <Button onClick={() => navigate(-1)}>Go back</Button>
-      </div>
-      {error && (
-        <div className="max-w-[400px]">
-          <Accordion>
-            <AccordionSummary expandIcon={<MdExpandMore />}>
-              <Typography variant="h6" className="line-clamp-1">
-                Detail: {error.status}, {error.statusText}.
-              </Typography>
-            </AccordionSummary>
-            <AccordionDetails>{error.data}</AccordionDetails>
-          </Accordion>
+    <div className="flex flex-col min-h-dvh">
+      <Header />
+      <main className="flex flex-col items-center justify-center flex-1 px-2 space-y-3 -translate-y-8 pt-header md:pt-header-md">
+        <Image src={noDataSVG} alt="Page not found" className="mt-4" />
+        <Typography variant="h2">{title}</Typography>
+        <div className="space-x-2 !mb-4">
+          <Button href={ROUTE_HOME}>Go to home</Button>
+          <Button onClick={() => navigate(-1)}>Go back</Button>
         </div>
-      )}
-      <Helmet>
-        <title>404 - Comic</title>
-      </Helmet>
-    </BasicLayout>
+        <Typography variant="body2" className="text-center">
+          If you have any questions, please contact us via{' '}
+          <Typography component="span" variant="inherit" copyable>
+            {EMAIL_HELPER}
+          </Typography>
+          .
+        </Typography>
+        {error && (
+          <div className="max-w-[400px]">
+            <Accordion>
+              <AccordionSummary expandIcon={<MdExpandMore />}>
+                <Typography variant="h6" className="line-clamp-1">
+                  Detail: {error.status}, {error.statusText}.
+                </Typography>
+              </AccordionSummary>
+              <AccordionDetails>{error.data}</AccordionDetails>
+            </Accordion>
+          </div>
+        )}
+        <Helmet>
+          <title>404 - Comic</title>
+        </Helmet>
+      </main>
+    </div>
   );
 }
 
