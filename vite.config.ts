bbb088174b168b61279a44340f3ffa3c1557eebc
Colpno <gvinhh@gmail.com>
2025-01-23 23:40:11 +0700

    define output in rollupOptions

--- vite.config.ts
+++ vite.config.ts
@@ -6,4 +6,23 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths(), babel({ filter: /\.tsx?$/ })],
+  build: {
+    rollupOptions: {
+      treeshake: true,
+      output: {
+        manualChunks: {
+          react: ['react'],
+          'react-dom': ['react-dom'],
+          '@mui/icons-material': ['@mui/icons-material'],
+          '@mui/material': ['@mui/material'],
+          '@mui/x-data-grid': ['@mui/x-data-grid'],
+          '@reduxjs/toolkit': ['@reduxjs/toolkit'],
+          'react-redux': ['react-redux'],
+          'swiper/react': ['swiper/react'],
+          'react-icons': ['react-icons'],
+          'react-hook-form': ['react-hook-form'],
+        },
+      },
+    },
+  },
 });
