7a7fd9a86ced6c3ed876719706b96e51b66d3528
Colpno <gvinhh@gmail.com>
2026-03-25 01:40:48 +0700

    refactor remove babel

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
-import babel from 'vite-plugin-babel';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths(), babel({ filter: /\.tsx?$/ })],
+  plugins: [react(), tsconfigPaths()],
   build: {
     rollupOptions: {
       treeshake: true,
